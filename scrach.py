062da0dea8884ade7948f8b9d977c19ed8bd40f2
Kae Sluder <kaesluder@fastmail.com>
2023-02-08 12:26:06 -0500

    working pywebview and pyinstaller

--- scrach.py
+++
@@ -1,4 +0,0 @@
-import webview
-
-window = webview.create_window("Woah dude!", "http://localhost:3000")
-webview.start()
---
+++ scratch.py
@@ -0,0 +1,19 @@
+import webview
+from app import app
+from flask_cors import CORS
+import threading
+from os import path
+
+path_to_dat = path.abspath(path.join(path.dirname(__file__), "gui/index.html"))
+
+if __name__ == "__main__":
+
+    print(path_to_dat)
+
+    CORS(app)
+
+    # recipe from https://thewebdev.info/2022/04/03/how-to-start-a-python-flask-application-in-separate-thread/
+    threading.Thread(target=lambda: app.run(debug=True, use_reloader=False)).start()
+
+    window = webview.create_window("Drama Llama!", path_to_dat)
+    webview.start()
