814daaf2fb07141233826b3a2abfc5f972e9b111
Kae Sluder <kaesluder@fastmail.com>
2023-02-11 21:26:16 -0500

    run filters on new items

--- app/input_pipeline.py
+++ app/input_pipeline.py
@@ -23,19 +23,35 @@ def get_sources():
     """
     db = Dl_db(DB_PATH)
     results = [feed.get("source") for feed in db.feeds()]
-    db.connection.close()
     return results
+
+
+def get_filters():
+    db = Dl_db(DB_PATH)
+    return db.load_filters()
+
+
+def apply_filter_all_entries():
+    db = Dl_db(DB_PATH)
+    cursor = db.connection.cursor()
+    entries = cursor.execute("select * from entries").fetchall()
+    filters = get_filters()
+    for filter in filters.values():
+        for entry in entries:
+            filter.analyze(dict(entry))
 
 
 def pipeline():
     """Refreshes all feeds from database."""
     db = Dl_db(DB_PATH)
+    filter_list = get_filters()
     for source in get_sources():
         data = rss.parse_source(source)
-        for filter in filter_list:
+        for filter in filter_list.values():
             for item in data["entries"]:
                 item = filter.analyze(item)
         db.upsert_RSS(data)
+    apply_filter_all_entries()
     db.connection.close()
 
 
